6381891143e08911aa8a4366b33a123d28f814c3
luis <luis.wehrberger2@gmail.com>
2025-01-09 11:59:33 +0100

    refactor: add sfml to vendor dir

--- premake5.lua
+++ premake5.lua
@@ -25,22 +25,22 @@ project "LeagueOfPixels"
       defines { "DEBUG" }
       symbols "On"
       postbuildcommands {
-         "{COPY} C:/dev/libs/SFML-2.6.2/bin/sfml-graphics-d-2.dll %{cfg.targetdir}",
-         "{COPY} C:/dev/libs/SFML-2.6.2/bin/sfml-window-d-2.dll %{cfg.targetdir}",
-         "{COPY} C:/dev/libs/SFML-2.6.2/bin/sfml-system-d-2.dll %{cfg.targetdir}",
-        --  "{COPY} C:/dev/libs/SFML-2.6.2/bin/sfml-audio-d-2.dll %{cfg.targetdir}",
-        --  "{COPY} C:/dev/libs/SFML-2.6.2/bin/sfml-network-d-2.dll %{cfg.targetdir}"
+         "{COPY} vendor/SFML-2.6.2/bin/sfml-graphics-d-2.dll %{cfg.targetdir}",
+         "{COPY} vendor/SFML-2.6.2/bin/sfml-window-d-2.dll %{cfg.targetdir}",
+         "{COPY} vendor/SFML-2.6.2/bin/sfml-system-d-2.dll %{cfg.targetdir}",
+        --  "{COPY} vendor/SFML-2.6.2/bin/sfml-audio-d-2.dll %{cfg.targetdir}",
+        --  "{COPY} vendor/SFML-2.6.2/bin/sfml-network-d-2.dll %{cfg.targetdir}"
       }
 
    filter "configurations:Release"
       defines { "NDEBUG" }
       optimize "On"
       postbuildcommands {
-         "{COPY} C:/dev/libs/SFML-2.6.2/bin/sfml-graphics-2.dll %{cfg.targetdir}",
-         "{COPY} C:/dev/libs/SFML-2.6.2/bin/sfml-window-2.dll %{cfg.targetdir}",
-         "{COPY} C:/dev/libs/SFML-2.6.2/bin/sfml-system-2.dll %{cfg.targetdir}",
-        --  "{COPY} C:/dev/libs/SFML-2.6.2/bin/sfml-audio-2.dll %{cfg.targetdir}",
-        --  "{COPY} C:/dev/libs/SFML-2.6.2/bin/sfml-network-2.dll %{cfg.targetdir}"
+         "{COPY} vendor/SFML-2.6.2/bin/sfml-graphics-2.dll %{cfg.targetdir}",
+         "{COPY} vendor/SFML-2.6.2/bin/sfml-window-2.dll %{cfg.targetdir}",
+         "{COPY} vendor/SFML-2.6.2/bin/sfml-system-2.dll %{cfg.targetdir}",
+        --  "{COPY} vendor/SFML-2.6.2/bin/sfml-audio-2.dll %{cfg.targetdir}",
+        --  "{COPY} vendor/SFML-2.6.2/bin/sfml-network-2.dll %{cfg.targetdir}"
       }
 
    prebuildcommands {
